eba1080e70311013e4e91c4d7a55781c1cc51ebf
Wiktor1903 <11997@kasprzak.edu.pl>
2025-10-13 11:44:12 +0200

    test testów

--- BlazorApp1/Data/ApplicationDbContext.cs
+++ BlazorApp1/Data/ApplicationDbContext.cs
@@ -14,12 +14,12 @@ namespace BlazorApp1.Data
         public DbSet<Plec> SlownikPlec { get; set; }
 
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Osoba>()
-                .ToTable("osoba");
+        //    modelBuilder.Entity<Osoba>()
+        //        .ToTable("osoba");
             modelBuilder.Entity<Plec>().HasData(
                 new Plec { Id = 1, Nazwa = "Mężczyzna" },
                 new Plec { Id = 2, Nazwa = "Kobieta" },
